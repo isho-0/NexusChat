f8289b031d0786187787fab46d5ac69684570ad2
isho-0 <isho010607@gmail.com>
2026-03-12 21:50:00 +0900

    feat: 헤더 및 라이브러리 설정

---
+++ include/Server.h
@@ -0,0 +1,31 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+#include <winsock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+class Server {
+public:
+    Server(int port);
+    ~Server();
+
+    void start();
+    void stop();
+
+private:
+    int port;
+    SOCKET listenSocket;
+    bool running;
+
+    std::vector<SOCKET> clientSockets;
+    std::mutex clientsMutex;
+};
+
+#endif
